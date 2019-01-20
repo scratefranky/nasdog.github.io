dc57d88451dbd1c015991bedbe8570f11e249dc6
scratefranky <franky-ma@qq.com>
2019-01-20 19:58:59 +0800

    Site updated: 2019-01-20 19:58:49

--- js/totop.js
+++ js/totop.js
@@ -1,13 +1,3 @@
-$(window).scroll(function() {
-    $(window).scrollTop() > $(window).height()*0.5 ? $("#rocket").addClass("show") : $("#rocket").removeClass("show");
-});
-
-$("#rocket").click(function() {
-    $("#rocket").addClass("launch");
-    $("html, body").animate({
-        scrollTop: 0
-    }, 1000, function() {
-        $("#rocket").removeClass("show launch");
-    });
-    return false;
-});
+// build time:Sun Jan 20 2019 19:30:49 GMT+0800 (GMT+08:00)
+$(window).scroll(function(){$(window).scrollTop()>$(window).height()*.5?$("#rocket").addClass("show"):$("#rocket").removeClass("show")});$("#rocket").click(function(){$("#rocket").addClass("launch");$("html, body").animate({scrollTop:0},1e3,function(){$("#rocket").removeClass("show launch")});return false});
+//rebuild by neat 
